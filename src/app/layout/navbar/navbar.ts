54d396da23fc84acef166689c69c83c7f128d66c
Petter Meave <pmeave@ine.gob.bo>
2026-09-08 22:05:17 -0400

    ajustes

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -31,7 +31,29 @@ export class Navbar {
   networkStatus$: Subscription = Subscription.EMPTY;
   connection = inject(ConnectionService)
   themeService = inject(ThemesService);
-
+notificaciones = [
+    {
+      id: 1,
+      titulo: 'Nuevo despacho registrado',
+      mensaje: 'Se registró el despacho N° 000152.',
+      fecha: 'Hace 5 minutos',
+      icono: 'pi pi-truck'
+    },
+    {
+      id: 2,
+      titulo: 'Certificado pendiente',
+      mensaje: 'Existe un certificado de calidad pendiente de revisión.',
+      fecha: 'Hace 15 minutos',
+      icono: 'pi pi-file'
+    },
+    {
+      id: 3,
+      titulo: 'Nueva recepción',
+      mensaje: 'La cisterna LP-1254 fue recepcionada en planta.',
+      fecha: 'Hace 30 minutos',
+      icono: 'pi pi-inbox'
+    }
+  ];
   abrirMenu() {
     console.log('navbar: click')
     this.toggleMenu.emit();
@@ -42,16 +64,30 @@ export class Navbar {
     this.themeService.toggleDarkMode();
 
   }
-   confirm(event: Event) {
-        this.confirmationService.confirm({
-            target: event.target as EventTarget,
-            message: 'Save your current process?',
-            accept: () => {
-                this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
-            },
-            reject: () => {
-                this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
-            }
-        });
-    }
+  mostrarNotificaciones(event: Event) {
+
+    this.confirmationService.confirm({
+      target: event.currentTarget as EventTarget,
+
+      // No queremos botones de confirmación
+      acceptVisible: false,
+      rejectVisible: false,
+
+      // Tampoco necesitamos mensaje estándar
+      message: ''
+    });
+  }
+
+  //  confirm(event: Event) {
+  //       this.confirmationService.confirm({
+  //           target: event.target as EventTarget,
+  //           message: 'Save your current process?',
+  //           accept: () => {
+  //               this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+  //           },
+  //           reject: () => {
+  //               this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+  //           }
+  //       });
+  //   }
 }
